a20e84e6c7ac07c0d03316bb17a67a7ea2b18f6f
benzxcvasdf <71902795+benzxcvasdf@users.noreply.github.com>
2023-03-18 05:46:26 +0000

    modified:   HomeWork_2/TypewriterEffect.js

--- HomeWork_2/TypewriterEffect.js
+++ HomeWork_2/TypewriterEffect.js
@@ -10,7 +10,7 @@ function random(min, max){ //random number for delay time
     return Math.floor(Math.random()* (max- min+ 1)) + min;
 }
 function playAudio(){ //play sound effects
-    audio.load();
+
     audio.play();
 }
 
