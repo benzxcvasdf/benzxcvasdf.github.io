aa0421e85c2922494b8f7ed01ed7db8293a047ca
benzxcvasdf <71902795+benzxcvasdf@users.noreply.github.com>
2023-03-18 05:11:18 +0000

    modified:   HomeWork_2/TypewriterEffect.js

--- HomeWork_2/TypewriterEffect.js
+++ HomeWork_2/TypewriterEffect.js
@@ -1,7 +1,5 @@
 const textDisplay = document.getElementById('textEffect');
 const phrases = ['There are too many people using it now, please wait. If waiting too long you can try to F5'];
-const audio = new Audio('sound.mp3');
-audio.muted = true;
 let i =0;
 let j =0;
 let currentPhrases = [];
@@ -10,17 +8,20 @@ let delay = 0;
 function random(min, max){ //random number for delay time
     return Math.floor(Math.random()* (max- min+ 1)) + min;
 }
+function playAudio(){ //play sound effects
+    const audio = new Audio('sound.mp3');
+    audio.muted = true;
+    audio.play();
+}
 
 function loop(){ //the loop for type effect
 
     textDisplay.innerHTML = currentPhrases.join(''); //put in space
-    audio.muted = false;
-    audio.play();//play sound effects
     if (i < phrases.length){
 
         if (j < phrases[i].length){
             currentPhrases.push(phrases[i][j]);
-
+            playAudio(); //play sound effects
             j++;
         }
 
@@ -30,7 +31,6 @@ function loop(){ //the loop for type effect
             j=0;
         }
     }
-    audio.muted = true;
     delay = random(100, 500);
     setTimeout(loop, delay);
 }
